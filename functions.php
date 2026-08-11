76b2ff44661a68d90b4b45e8953b1baf1087d6e8
ALEX\alepc <alepcharitidis@gmail.com>
2026-08-11 14:59:17 +0300

    Add theme CSS and Javascript assets

--- functions.php
+++ functions.php
@@ -1,9 +1,27 @@
 <?php
+
 function nova_agency_enqueue_assets() {
+
     wp_enqueue_style(
         'nova-agency-style',
         get_stylesheet_uri()
     );
+
+    wp_enqueue_style(
+        'nova-agency-main',
+        get_template_directory_uri() . '/assets/css/main.css'
+    );
+
+    wp_enqueue_script(
+        'nova-agency-main-js',
+        get_template_directory_uri() . '/assets/js/main.js',
+        array(),
+        null,
+        true
+    );
 }
 
-add_action('wp_enqueue_scripts', 'nova_agency_enqueue_assets');
+add_action(
+    'wp_enqueue_scripts',
+    'nova_agency_enqueue_assets'
+);
